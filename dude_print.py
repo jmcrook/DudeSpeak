57b46d3de11035ac543ccd62583450ac73eccf15
jmcrook <jmcrook95@gmail.com>
2019-04-08 13:01:32 -0400

    added better detokenization of the pixel text, simplified the printing a little bit too.

--- dude_print.py
+++ dude_print.py
@@ -8,49 +8,56 @@ from dude_speak import ngram_model, weighted_random_by_dct
 words = nltk.tokenize.word_tokenize(open('dude_lines.txt', 'r').read().lower())
 m = ngram_model(words, 3)
 
-
 def detokenize(tokens):
-    return ''.join([' ' + i if i[0] not in string.punctuation and '\'' not in i else i for i in tokens])
+    return ''.join([' ' + i if i not in string.punctuation and '\'' not in i else i for i in tokens])
 
 def word_face(model, pic_file):
 
     pic = cv2.imread(pic_file, cv2.IMREAD_REDUCED_GRAYSCALE_2)
 
+    ## finding the face
     haar_cascade_face = cv2.CascadeClassifier(
             '/Users/jmcrook/Library/Python/2.7/lib/python/site-packages/cv2/data/haarcascade_frontalface_default.xml')
     faces_rects = haar_cascade_face.detectMultiScale(pic, scaleFactor=1.2, minNeighbors=5)
-
     x, y, w, h = faces_rects[0]
     face = pic[y-(h/3):y+h+(h/3), x-(w/3):x+w+(w/3)]
 
-    counter = 0
-    line = ''
-    word = []
+    ## generating text to be used as black pixels
+    num_black_pixels = sum([sum([1 for j in i if j >= 80]) for i in face])
+    char_pixels = ''
     curr_state = random.choice(model.keys())
-    for e in np.nditer(face):
+    while num_black_pixels > 0:
+        try:
+            curr_word = weighted_random_by_dct(model[curr_state])
+        except KeyError:
+            bad_state = curr_state
+            while curr_state == bad_state:
+                curr_state = random.choice(model.keys())
+            curr_word = weighted_random_by_dct(model[curr_state])
+        curr_state = tuple([curr_state[1], curr_word])
+        to_add = \
+            (' ' + curr_word if curr_word[0] not in string.punctuation and '\'' not in curr_word else curr_word)
+        num_black_pixels -= len(to_add)
+        char_pixels += to_add
+
+    #printing image line by line
+    counter = 0
+    char_pixels = list(char_pixels)
+    char_pixels.reverse()
+
+    line = ''
+    for p in np.nditer(face):
         if counter == face.shape[1]:
             counter = 0
             print line
             line = ''
 
-        if e < 80:
+        if p < 80:
             line += ' '
         else:
-            if not word:
-                line += ' '
-                try:  # in case curr_state is last two words of the corpus
-                    curr_word = weighted_random_by_dct(model[curr_state])
-                except e:
-                    bad_state = curr_state
-                    while curr_state == bad_state:
-                        curr_state = random.choice(model.keys())
-                    curr_word = weighted_random_by_dct(model[curr_state])
-                curr_state = tuple([curr_state[1], curr_word])
-                word = list(curr_word)
-                word.reverse()
-            else:
-                line += word.pop()
+            line += char_pixels.pop()
 
         counter += 1
+
 
 word_face(m, 'The_Dude.jpg')
